7864e224a9b27b758837c3bc54aed02c713204f4
wtysos11 <wtysos11@163.com>
2019-02-12 23:09:30 +0800

    冬木X-G刷胸骨，队伍豆爸弓凛尼托

--- main.py
+++ main.py
@@ -8,55 +8,61 @@ AvailableRegion = (44,29,1791,1009)
 pyautogui.PAUSE = 3
 
 player = autoPlayer((44,29),(1835,1038))
-#点击外层关卡
-pyautogui.click('aim.png')
-#通过过度界面，进入选人界面
-## 检查选人界面是否正常
-while not player.checkChooseHelp():
-    print("checking")
+for i in range(9):
+    #点击外层关卡
+    pyautogui.click('aim.png')
+    #通过过度界面，进入选人界面
+    ## 检查选人界面是否正常
+    while not player.checkChooseHelp():
+        print("checking")
+        time.sleep(1)
+    
+    print("Pass")
+    #player.findAddServant()
+    pyautogui.click(440,409)
+    player.beginMission()
+    #通过选人界面，进入第一面
+    while not player.checkBattleAvailable():
+        time.sleep(1)
+    
+    time.sleep(3)
+    player.chooseServentSkill(0,0)
     time.sleep(1)
-
-print("Pass")
-#player.findAddServant()
-pyautogui.click(440,409)
-player.beginMission()
-#通过选人界面，进入第一面
-while not player.checkBattleAvailable():
+    player.InBattle()
+    player.InBattlePhantasm(0)
+    player.InBattleAttack(0)
+    player.InBattleAttack(1)
+    
+    #通过选人界面，进入第二面
+    while not player.checkBattleAvailable():
+        time.sleep(1)
+    
+    time.sleep(3)
+    player.chooseServentSkill(1,1)
     time.sleep(1)
-
-time.sleep(3)
-player.chooseServentSkill(0,0)
-player.InBattle()
-player.InBattlePhantasm(0)
-player.InBattleAttack(0)
-player.InBattleAttack(1)
-
-#通过选人界面，进入第二面
-while not player.checkBattleAvailable():
+    player.InBattle()
+    player.InBattlePhantasm(1)
+    player.InBattleAttack(0)
+    player.InBattleAttack(1)
+    #通过选人界面，进入第三面
+    while not player.checkBattleAvailable():
+        time.sleep(1)
+    
+    time.sleep(3)
+    player.chooseServentSkill(2,1)
     time.sleep(1)
-
-time.sleep(3)
-player.chooseServentSkill(1,1)
-player.InBattle()
-player.InBattlePhantasm(1)
-player.InBattleAttack(0)
-player.InBattleAttack(1)
-#通过选人界面，进入第三面
-while not player.checkBattleAvailable():
-    time.sleep(1)
-
-time.sleep(3)
-player.chooseServentSkill(2,1)
-player.InBattle()
-player.InBattlePhantasm(2)
-player.InBattleAttack(0)
-player.InBattleAttack(1)
-time.sleep(30)
-#第三面结束，进入结算界面
-pyautogui.PAUSE = 1.5
-pyautogui.click(1538,958)
-pyautogui.click(1538,958)
-pyautogui.click(1538,958)
-pyautogui.click(1538,958)
-pyautogui.click(1538,958)
-pyautogui.click(1538,958)
+    player.InBattle()
+    player.InBattlePhantasm(2)
+    player.InBattleAttack(0)
+    player.InBattleAttack(1)
+    time.sleep(50)
+    #第三面结束，进入结算界面
+    pyautogui.PAUSE = 1.5
+    pyautogui.click(1538,958)
+    pyautogui.click(1538,958)
+    pyautogui.click(1538,958)
+    time.sleep(3)
+    pyautogui.click(1538,958)
+    pyautogui.click(1538,958)
+    pyautogui.click(1538,958)
+    time.sleep(10)
