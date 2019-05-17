66a1c165d95789b3c6bd0b47c734681610118c99
wtysos11 <wtysos11@163.com>
2019-05-17 21:59:39 +0800

    完成QP本

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pyautogui.PAUSE = 3
 
 player = autoPlayer((44,29),(1835,1038))
 for i in range(6):
-    #塞勒姆伽罗之丘，刷铁钉。配置弓凛尼托三藏+孔明，换人礼装
+    #周五，QP之门40AP。使用船长，阿福和金时，礼装为50NP礼装
     location = pyautogui.locateOnScreen('aim.png',confidence = 0.95)
     pyautogui.click(pyautogui.center(location))
     #检查是否需要苹果
@@ -25,18 +25,17 @@ for i in range(6):
     time.sleep(2)
     #pyautogui.click(440,409)
     player.beginMission()
-    #通过选人界面，进入第一面。第一面弓凛一技能，弓凛宝具。
+    #通过选人界面，进入第一面。第一面阿福三技能
     while not player.checkBattleAvailable():
         time.sleep(1)
     
     pyautogui.PAUSE = 5
     time.sleep(3)
-    player.chooseServentSkill(0,0)
-    player.chooseServentSkill(0,1)
+    player.chooseServentSkill(1,2)
     time.sleep(2)
     player.InBattle()
     pyautogui.PAUSE = 0.5
-    player.InBattlePhantasm(0)
+    player.InBattlePhantasm(1)
     player.InBattleAttack(0)
     player.InBattleAttack(1)
     pyautogui.PAUSE = 3
@@ -46,31 +45,27 @@ for i in range(6):
     pyautogui.click(196,491)
 
 
-    #通过选人界面，换人礼装，弓凛换孔明，尼托宝具
+    #第二面，船长一技能，艾什二、三技能
     while not player.checkBattleAvailable():
         time.sleep(1)
     
     pyautogui.PAUSE = 5
-    time.sleep(3)
-    player.chooseMasterSkill(2)
-    player.changePeopleSkill(0,3)
-    #孔明充能尼托
-    player.chooseServentSkill(0,1)
-    player.chooseServentSkill(0,2)
-    player.chooseServentSkillWithSpecificAim(0,0,1)
-    player.chooseServentSkill(1,0)
+    time.sleep(2)
+    player.chooseServentSkill(0,0)
+    player.chooseServentSkill(2,1)
+    player.chooseServentSkill(2,2)
     pyautogui.PAUSE = 3
 
     player.InBattle()
     pyautogui.PAUSE = 0.5
-    player.InBattlePhantasm(1)
+    player.InBattlePhantasm(2)
     player.InBattleAttack(0)
     player.InBattleAttack(1)
     pyautogui.PAUSE = 3
 
     inBattle3 = True
     #检查是否进入第三面
-    #通过选人界面，进入第三面。尼托宝具，三藏宝具
+    #通过选人界面，进入第三面。船长三技能
     '''
     while not inBattle3:
         while not player.checkBattleAvailable():
@@ -95,18 +90,16 @@ for i in range(6):
     #print('the third available')    
     time.sleep(3)
     pyautogui.PAUSE = 3
-    player.chooseMasterSkill(0)
-    player.chooseServentSkill(1,1)
-    player.chooseServentSkill(2,0)
+    player.chooseServentSkill(0,2)
     player.chooseEnemy(1)
 
     time.sleep(2)
     
     player.InBattle()
     pyautogui.PAUSE = 0.5
-    player.InBattlePhantasm(1)
-    player.InBattlePhantasm(2)
+    player.InBattlePhantasm(0)
     player.InBattleAttack(0)
+    player.InBattleAttack(1)
     pyautogui.PAUSE = 3
     time.sleep(45)
     pyautogui.click(196,491)
